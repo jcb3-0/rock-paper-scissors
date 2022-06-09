637e349d9a1daf8805ca994fb8c8245616becb01
Jason Bauzon <jasonbauzon@gmail.com>
2022-06-09 13:28:49 -0500

    create variables and function computerPlay()

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 "use strict"
 
-/*
-Create a variable to store computersPlay.
-Create a variable to store usersPlay.
-Create variable for roundNumber [].
-    - initVal = 1
-Generate a random play from computer to store in computersPlay.
-    - Math.floor(Math.random() * 3) to get random number.
-    - assign random number to rock, paper, scissors
-Get input from user to store in usersPlay via prompt.
-Compare computersPlay to usersPlay.
-    - convert usersPlay toLowerCase.
-    - rock > scissors
-    - scissors > paper
-    - paper > rock
-    - If userWon ? true : false
-Store round n outcome (userWon) to roundNumber variable.
-Tell user usersPlay, computersPlay, and rounds won/lost/tied via alert.
-If round n <= 5, move to next round.
-Else tell user the outcome of the game.
-    - If user wins/loses 3 rounds, tell user rounds won, and you won/lost game via alert.
-*/
+
+//Create a variable to store computerSelection.
+//let computerSelection = computerPlay(); This generated one random value without chaging to a new random.
+//Create a variable to store userSelection.
+let userSelection = userPlay;
+//Create variable for roundNumber [].
+//    - initVal = 1
+let roundNumber = [];
+//Generate a random play from computer to store in computerSelection.
+//    - Math.floor(Math.random() * 3) to get random number.
+//    - assign random number to rock, paper, scissors
+function computerPlay() {
+    let n = Math.floor(Math.random() * 3);
+        switch (n) {
+            case 0:
+            return "rock";
+            break;
+            case 1:
+            return "paper";
+            break;
+            case 2:
+            return "scissors";
+            break;
+        }
+}
+
+//Get input from user to store in usersPlay via prompt.
+//Compare computersPlay to usersPlay.
+//    - convert usersPlay toLowerCase.
+//    - rock > scissors
+//    - scissors > paper
+//    - paper > rock
+//    - If userWon ? true : false
+//Store round n outcome (userWon) to roundNumber variable.
+//Tell user usersPlay, computersPlay, and rounds won/lost/tied via alert.
+//If round n <= 5, move to next round.
+//Else tell user the outcome of the game.
+//    - If user wins/loses 3 rounds, tell user rounds won, and you won/lost game via alert.
